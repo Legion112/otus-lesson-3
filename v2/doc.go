c7b9ebb8449925567d8614f1834c3d32c0cd7cbc
mirecl <grazhdankov@gmail.com>
2019-09-12 13:09:16 +0300

    fix doc

--- v2/doc.go
+++ v2/doc.go
@@ -8,5 +8,21 @@ Optional: support for the escape sequences
 	*qwe\4\5 => qwe45 (*)
 	*qwe\45 => qwe44444 (*)
 	*qwe\\5 => qwe\\\\\ (*)
+
+Example used:
+	package main
+
+	import (
+		"fmt"
+		unpack "github.com/mirecl/otus-lesson-3/v2"
+	)
+
+	func main() {
+		if data, err := pack.Unpack("a4"); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(data)
+		}
+	}
 */
 package unpack
